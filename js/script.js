67449bdabe2acecb09f603dc073e704c3d7de8bc
SimoneBonvecchio84 <simonebonvecchio@gmail.com>
2024-04-29 17:01:57 +0200

    Feat: bonus

--- js/script.js
+++ js/script.js
@@ -4,24 +4,31 @@ createApp ({
     data() {
         return{
            listMail : [], 
+           isLoading: false
         }
     },
-    created () {
-        this.sendMeMail ()
-    },
+    // created () {
+    //    this.sendMeMail(); 
+    // },
     methods: {
        sendMeMail: function () {
-        for(let i = 0; i < 10; i++) {
-            
-            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-            .then((resp) =>{
-               console.log(resp)
-               this.listMail.push(resp.data.response)
-               console.log(this.listMail)
-            });
-        }
-        
-
+            this.isLoading = true; 
+            for(let i = 0; i < 10; i++) {            
+                axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) =>{                    
+                    
+                    this.listMail.push(resp.data.response);     
+                    console.log(this.listMail);
+                    if(this.listMail.length === 10) {
+                        this.isLoading = false;
+                        
+                    }
+                });  
+            } 
+       },
+       
+       reset: function () {
+        this.listMail = [];
        } 
     }
 }).mount("#app");
